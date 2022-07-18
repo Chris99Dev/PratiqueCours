8af065bd3c701ec94d0a25f74600c46aa3974015
Christopher Ferreira <ferrchristopher@gmail.com>
2022-07-18 12:47:40 +0200

    Token Creation
    Instaling the Tokenwebtoken to the project
    Adding the Token sign to the creation for user

--- P6 - API/ProjectP6/backend/controllers/user.js
+++ P6 - API/ProjectP6/backend/controllers/user.js
@@ -1,4 +1,6 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 const { findOne } = require('../models/User');
 
 const User = require('../models/User');
@@ -30,7 +32,11 @@ exports.login = (req, res, next) => {
                         } else {
                             res.status(200).json({
                                 userId: user._id,
-                                token: 'TOKEN'
+                                token: jwt.sign(
+                                    { userId: user._id },
+                                    'RANDOM_TOKEN_SECRET',
+                                    { expiresIn: '24h' }
+                                )
                             });
                         }
                     })
